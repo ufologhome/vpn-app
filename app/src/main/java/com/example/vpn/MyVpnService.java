bff62fdb3335418a8bd83d2eca37a9c01a4520d4
ufologhome <38780109+ufologhome@users.noreply.github.com>
2025-12-29 08:51:42 +0300

    Update MyVpnService.java

--- app/src/main/java/com/example/vpn/MyVpnService.java
+++ app/src/main/java/com/example/vpn/MyVpnService.java
@@ -1,41 +1,27 @@
 package com.example.vpn;
 
 import android.net.VpnService;
+import android.content.Intent;
 import android.os.ParcelFileDescriptor;
-import android.util.Log;
-import java.io.IOException;
 
 public class MyVpnService extends VpnService {
 
-    private ParcelFileDescriptor vpnInterface;
-    private TunnelThread tunnelThread;
-    private static final String TAG = "MyVpnService";
+    ParcelFileDescriptor tun;
 
     @Override
-    public void onCreate() {
-        super.onCreate();
+    public int onStartCommand(Intent intent, int flags, int startId) {
+
         Builder builder = new Builder();
-        builder.addAddress("10.0.0.2", 32);
-        builder.addRoute("0.0.0.0", 0);
-        builder.setMtu(1400);
+        builder.setSession("WG-Lite")
+                .addAddress("10.0.0.2", 32)
+                .addRoute("0.0.0.0", 0);
 
-        try {
-            vpnInterface = builder.establish();
-            tunnelThread = new TunnelThread(vpnInterface.getFileDescriptor());
-            tunnelThread.start();
-        } catch (Exception e) {
-            Log.e(TAG, "VPN initialization error", e);
-        }
-    }
+        tun = builder.establish();
 
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
-        try {
-            if (tunnelThread != null) tunnelThread.interrupt();
-            if (vpnInterface != null) vpnInterface.close();
-        } catch (IOException e) {
-            Log.e(TAG, "Error closing VPN", e);
-        }
+        MainActivity.setStatus("VPN запущен");
+
+        new Thread(new TunnelThread(tun.getFileDescriptor())).start();
+
+        return START_STICKY;
     }
 }
