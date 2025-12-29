6542060e3add9625c017fa148a6709453e910654
ufologhome <38780109+ufologhome@users.noreply.github.com>
2025-12-29 21:48:51 +0300

    Update MainActivity.java

--- app/src/main/java/com/example/vpn/MainActivity.java
+++ app/src/main/java/com/example/vpn/MainActivity.java
@@ -2,7 +2,6 @@ package com.example.vpn;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.net.VpnService;
 import android.os.Bundle;
 import android.widget.TextView;
 import android.view.View;
@@ -25,12 +24,7 @@ public class MainActivity extends Activity {
     }
 
     public void startVpn(View v) {
-        Intent intent = VpnService.prepare(this);
-        if (intent != null) {
-            startActivityForResult(intent, 1);
-        } else {
-            startService(new Intent(this, MyVpnService.class));
-        }
+        startService(new Intent(this, MyVpnService.class));
     }
 
     public void stopVpn(View v) {
